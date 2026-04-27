eb60e70f18006f6a1b53598173261c32098c55ac
Sagar Roy <roy.sagar.1989@gmail.com>
2026-04-27 19:17:10 +1000

    Github actions workflow and dispatch options

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,17 +2,6 @@
 import { defineConfig, devices } from '@playwright/test';
 import path from "path";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   //below line logs into the application and create the storageState json for different users
   globalSetup: require.resolve("./config/global-setup"),
@@ -22,18 +11,15 @@ export default defineConfig({
   reporter: [
     ['html', { open: 'never' }]
   ],
-  workers: 5,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 1,
   timeout: 30 * 1000,
   expect: {
     timeout: 30 * 1000,
   },
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: process.env.HEADLESS !== 'false',
+    headless: process.env.HEADED !== 'true',
   },
 
   /* Configure projects for major browsers */
@@ -42,17 +28,14 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 
 });
